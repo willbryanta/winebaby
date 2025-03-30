2017006370fd240bcc9b07ceba57ea5b26853d86
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-03-30 17:40:01 +1100

    add: connect to postgresdb

--- app/src/backend/cmd/server/main.go
+++ app/src/backend/cmd/server/main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"winebaby/internal/routes"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+func connectDB() (*sql.DB, error){
+	connStr := "postgres://"+os.Getenv("DB_USER") + ":" +
+	os.Getenv("DB_PASSWORD") + "@" +
+	os.Getenv("DB_HOST") + ":" +
+	os.Getenv("DB_PORT") + "/" +
+	os.Getenv("DB_NAME") + "?sslmode=disable"
+	return sql.Open("postgres", connStr)
+}
+
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, falling back to environment variables")
+	}
+
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal("Error pinging the database: ", err)
+	}
+	log.Println("Successfully connected to winebaby_db!")
+
 	r := chi.NewRouter()
 	r.Mount("/", routes.RegisterRoutes())
 
