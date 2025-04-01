dbbda6b338011901b89dff2eebd4c0cc3ed9b3a7
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-04-01 12:43:56 +1100

    add: review form

--- app/src/components/ReviewForm/ReviewForm.tsx
+++ app/src/components/ReviewForm/ReviewForm.tsx
@@ -1,16 +1,121 @@
-import Form from "next/form";
+"use client";
+
+import { useState, FormEvent, ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
+
+interface FormData {
+  WineID: number | null;
+  Winemaker: string;
+  Title: string;
+  Description: string;
+  Rating: number;
+}
 
 export default function ReviewForm() {
+  const router = useRouter();
+
+  const [formData, setFormData] = useState<FormData>({
+    WineID: 0,
+    Winemaker: "",
+    Title: "",
+    Description: "",
+    Rating: 0,
+  });
+
+  const onFormInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+
+    const updatedValue =
+      name === "WineID"
+        ? value === ""
+          ? null
+          : Number(value)
+        : name === "Rating"
+        ? Number(value)
+        : value;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: updatedValue,
+    }));
+  };
+
+  const isFormValid = (): boolean => {
+    const { WineID, Winemaker, Title, Description, Rating } = formData;
+    return (
+      (WineID !== null || WineID !== 0) &&
+      Winemaker.trim() !== "" &&
+      Title.trim() !== "" &&
+      Description.trim() !== "" &&
+      Rating >= 0 &&
+      Rating <= 5
+    );
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!isFormValid()) {
+      alert("Please fill out all fields correctly.");
+      return;
+
+      try {
+        // Update once the review service has been built
+        const res = await fetch("/api/review", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (!res.ok) throw new Error("Failed to submit review");
+        alert("Review submitted successfully!");
+        router.push("/");
+      } catch (err) {
+        console.error("Submission error:, err as unknown");
+        alert("An error occured while submitting your review");
+      }
+    }
+  };
+
+  //REVIEW - Revisit this, the user shouldn't be passing the title or winemaker of the wine, it should be automatically populated from the DB based on WineID
   return (
     <>
-      <Form action="/review">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Winemaker">Winemaker</label>
+        <input
+          id="Winemaker"
+          name="Winemaker"
+          value={formData.Winemaker}
+          onChange={onFormInputChange}
+          placeholder="Enter Winemaker"
+        ></input>
         <label htmlFor="Title">Title</label>
-        <input name="title"></input>
-        <label htmlFor="Date">Date</label>
-        <input name="date"></input>
-        <label htmlFor="Review">Review</label>
-        <input name="review"></input>
-      </Form>
+        <input
+          id="Title"
+          name="Title"
+          value={formData.Title}
+          onChange={onFormInputChange}
+          placeholder="Enter Title"
+        ></input>
+        <label htmlFor="Description">Description</label>
+        <input
+          id="Description"
+          name="Description"
+          value={formData.Description}
+          onChange={onFormInputChange}
+          placeholder="Enter Description"
+        ></input>
+        <label htmlFor="Rating">Rating</label>
+        <input
+          id="Rating"
+          name="Rating"
+          value={formData.Rating}
+          onChange={onFormInputChange}
+          placeholder="Enter Rating"
+        ></input>
+      </form>
     </>
   );
 }
