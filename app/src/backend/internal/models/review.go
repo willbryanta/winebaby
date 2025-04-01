49999f8711ba5c13d8f06399fbca3b94d44a809b
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-04-01 12:39:15 +1100

    add: review form

--- app/src/backend/internal/models/review.go
+++ app/src/backend/internal/models/review.go
@@ -3,7 +3,7 @@ package models
 type Review struct {
 	ID				int 	`json:"id"`
 	WineID 			int		`json:"wine_id"`
-	Manufacturer	string	`json:"maker"`
+	Winemaker		string	`json:"winemaker"`
 	Title			string 	`json:"title"`
 	Description 	string 	`json:"description"`
 	Rating			int 	`json:"rating"`
