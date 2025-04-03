718e49995fb5331ae954f5e56714413c074bdcc3
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-04-03 12:42:20 +1100

    edit: signin form styling

--- app/src/components/SignInForm/SignInForm.tsx
+++ app/src/components/SignInForm/SignInForm.tsx
@@ -4,12 +4,21 @@ import Form from "next/form";
 
 export default function SignInForm() {
   return (
-    <Form action="/signin">
-      <label htmlFor="username">Username</label>
-      <input name="username"></input>
-      <label htmlFor="password">Password</label>
-      <input name="password"></input>
-      <button type="submit">Sign in</button>
+    <Form
+      action="/signin"
+      className="bg-white p-6 rounded-lg shadow-md w-80 space-y-4"
+    >
+      <label htmlFor="username" className="block text-gray-700">
+        Username
+      </label>
+      <input name="username" placeholder="Username"></input>
+      <label htmlFor="password" className="block text-gray-700">
+        Password
+      </label>
+      <input name="password" placeholder="Password"></input>
+      <button type="submit" className="btn">
+        Sign in
+      </button>
     </Form>
   );
 }
