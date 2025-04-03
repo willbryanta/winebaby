c3d8817f602770e4b05cfe2a80d1137285062e5b
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-04-03 12:38:23 +1100

    edit: sign up form styling

--- app/src/components/SignUpForm/SignUpForm.tsx
+++ app/src/components/SignUpForm/SignUpForm.tsx
@@ -4,12 +4,17 @@ import Form from "next/form";
 
 export default function SignUpForm() {
   return (
-    <Form action="signup">
-      <input name="email"></input>
-      <input name="username"></input>
-      <input name="password"></input>
-      <input name="confirm-password"></input>
-      <button type="submit">Sign up</button>
+    <Form
+      action="signup"
+      className="bg-white p-6 rounded-lg shadow-md w-80 space-y-4"
+    >
+      <input name="email" placeholder="Email"></input>
+      <input name="username" placeholder="Username"></input>
+      <input name="password" placeholder="Password"></input>
+      <input name="confirm-password" placeholder="Confirm Password"></input>
+      <button type="submit" className="btn">
+        Sign up
+      </button>
     </Form>
   );
 }
