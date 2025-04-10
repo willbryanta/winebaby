be4003e939e3f0ed3a7e0889340250e4766cd957
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-04-10 12:52:10 +1000

    add: User routes to registerroutes

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,9 +1,10 @@
 package routes
 
 import (
-	"github.com/go-chi/chi/v5"
-	"winebaby/internal/handlers"
 	"net/http"
+	"winebaby/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func RegisterRoutes() *chi.Mux {
@@ -20,6 +21,20 @@ func RegisterRoutes() *chi.Mux {
 	r.Post("/reviews", handlers.CreateReview)
 	r.Put("/reviews/{id}", handlers.UpdateReview)
 	r.Delete("/reviews/{id}", handlers.DeleteReview)
+
+	r.Post("/signup", handlers.SignUp)
+	r.Post("/signin", handlers.SignIn)
+	r.Get("/user/{username}", handlers.GetUserProfile)
+	r.Put("/user/{username}", handlers.UpdateUserProfile)
+	r.Delete("/user/{username}", handlers.DeleteUser)
+	r.Get("/user/{username}/wines", handlers.GetUserFavoriteWines)
+	r.Post("/user/{username}/wines", handlers.AddUserFavoriteWine)
+	r.Delete("/user/{username}/wines/{wineId}", handlers.RemoveUserFavoriteWine)
+	r.Get("/user/{username}/reviews", handlers.GetUserReviews)
+	r.Post("/user/{username}/reviews", handlers.CreateUserReview)
+	r.Put("/user/{username}/reviews/{reviewId}", handlers.UpdateUserReview)
+	r.Delete("/user/{username}/reviews/{reviewId}", handlers.DeleteUserReview)
+	r.Get("/user/{username}/reviews/{reviewId}", handlers.GetUserReviewById)
 
 	return r
 }
