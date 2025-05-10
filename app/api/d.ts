04c37ecf1b981ca410f2e8f857212be9782f6880
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-05-10 13:06:16 +1000

    edit: import meta

--- app/api/d.ts
+++ app/api/d.ts
@@ -1,7 +1,6 @@
-
 /// <reference types="vite/client" />
 
 export interface ImportMetaEnv {
-    readonly VITE_BACKEND_URL: string;
-    readonly VITE_JWT_KEY: string;
-  }
+  readonly VITE_BACKEND_URL: string;
+  readonly VITE_JWT_KEY: string;
+}
