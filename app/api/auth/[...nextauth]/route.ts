016a7ada4b1297d241aa4ac89564e5c08e68977c
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-05-12 13:48:33 +1000

    del: route.ts

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./auth";
-
-// Initialize the NextAuth handler
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
-export default NextAuth(authOptions);
