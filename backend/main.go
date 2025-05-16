7857128e0fb63e2de973f22ed7af721c8bd9e5f0
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-05-16 12:55:14 +1000

    del: remove unneeded init func from main go

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -35,71 +34,12 @@ func connectDB() (*sql.DB, error){
 	return db, nil
 }
 
-// TODO: Move this to a separate file potentially
-func initSchema(db *sql.DB) error {
-	queries := []string{
-		`CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			username VARCHAR(50) UNIQUE NOT NULL,
-			password TEXT NOT NULL,
-			email VARCHAR(100) UNIQUE,
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		)`,
-		`CREATE TABLE IF NOT EXISTS wines (
-			id SERIAL PRIMARY KEY,
-			name VARCHAR(100) NOT NULL,
-			year INT,
-			manufacturer VARCHAR(100),
-			region VARCHAR(100),
-			alcohol_content FLOAT,
-			serving_temp FLOAT,
-			serving_size FLOAT,
-			serving_size_unit VARCHAR(50),
-			serving_size_unit_abbreviation VARCHAR(10),
-			serving_size_unit_description VARCHAR(100),
-			serving_size_unit_description_abbreviation VARCHAR(10),
-			serving_size_unit_description_plural VARCHAR(100),
-			price FLOAT,
-			rating FLOAT,
-			type VARCHAR(50),
-			colour VARCHAR(50),
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		)`,
-		`CREATE TABLE IF NOT EXISTS favorite_wines (
-			user_id INT REFERENCES users(id) ON DELETE CASCADE,
-			wine_id INT REFERENCES wines(id) ON DELETE CASCADE,
-			PRIMARY KEY (user_id, wine_id),
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		)`,
-		`CREATE TABLE IF NOT EXISTS reviews (
-			id SERIAL PRIMARY KEY,
-			user_id INT REFERENCES users(id) ON DELETE CASCADE,
-			wine_id INT REFERENCES wines(id) ON DELETE CASCADE,
-			comment TEXT,
-			review_date VARCHAR(50),
-			review_date_time VARCHAR(50),
-			review_date_time_utc VARCHAR(50),
-			title VARCHAR(100),
-			description TEXT,
-			rating INT,
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		)`,
-	}
-
-	for _, query := range queries {
-		_, err := db.Exec(query)
-		if err != nil {
-			return fmt.Errorf("error executing schema query: %w", err)
-		}
-	}
-	return nil
-}
-
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 
 		if r.Method == "OPTIONS" {
