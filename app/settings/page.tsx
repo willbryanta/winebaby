8d57168bbf061cb56ae9041e299a5d282226ca08
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-06-07 15:18:47 +1000

    edit: settings page updartes

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -8,8 +8,8 @@ export default function SettingsPage() {
       <div className="bg-white p-6 rounded shadow-md w-96">
         <h1 className="text-2xl font-bold mb-4 text-wine">Settings</h1>
         <li>
-          <a href="/userProfile" className="text-blue-500 hover:underline">
-            User Profile
+          <a href="/userSettings" className="text-blue-500 hover:underline">
+            User Settings
           </a>
         </li>
         <li>
