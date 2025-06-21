370cab9f51b1de5c8db777d05d4cfdcba4538f5c
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-06-21 19:06:15 +1000

    edit: dashboard pass auth state

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
 "use client";
 import React from "react";
 import NavBar from "./api/components/NavBar";
+import { checkSession } from "./api/auth/services/sessionService";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+  const [isAuth, setIsAuth] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+  useEffect(() => {
+    const verifySession = async () => {
+      try {
+        const session = await checkSession();
+        setIsAuth(!!session);
+      } catch {
+        setIsAuth(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    verifySession();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        Loading...
+      </div>
+    );
+  }
+  if (!isAuth) {
+    return (
+      <p className="flex items-center justify-center h-screen">Access Denied</p>
+    );
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col bg-gray-100">
-      <NavBar />
+      <NavBar isAuth={isAuth} />
       <div className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-[4.8rem] font-bold text-wine">
           Welcome to WineBaby
