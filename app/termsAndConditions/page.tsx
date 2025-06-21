4699f0710d1eeae2ed7ab251ec32d134005eca24
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2025-06-21 17:56:14 +1000

    edit: update ts and cs view to use checksession service

--- app/termsAndConditions/page.tsx
+++ app/termsAndConditions/page.tsx
@@ -1,9 +1,31 @@
 import NavBar from "../api/components/NavBar";
+import { checkSession } from "../api/auth/services/sessionService";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function TermsAndConditions() {
+  const router = useRouter();
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+
+  useEffect(() => {
+    const verifySession = async () => {
+      try {
+        const session = await checkSession();
+        if (!session) {
+          setIsAuth(false);
+          router.push("/signin");
+        }
+        setIsAuth(true);
+      } catch (error) {
+        throw new Error(`Session verification failed: ${String(error)}`);
+      }
+    };
+    verifySession();
+  }, [router]);
   return (
     <div>
-      <NavBar />
+      <NavBar isAuth={isAuth} />
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-3xl">
           <h1 className="text-2xl font-bold mb-4">Terms and Conditions</h1>
